365ea28ddcc4dfb0f459f181af65907f756c6b78
SkiboPavlo <skibo@i.ua>
2017-04-19 22:40:40 +0300

    add attachments for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   has_many :task_lists, dependent: :destroy
   has_many :tasks, through: :task_lists, dependent: :destroy
   has_many :tags, dependent: :destroy
+  mount_uploader :attachment, ProjectUploader # Tells rails to use this uploader for this model.
   belongs_to :user
 end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,6 @@ class Task < ApplicationRecord
   belongs_to :task_list
   has_one :project, through: :task_list
   has_and_belongs_to_many :tags
-  mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
+  mount_uploader :attachment, TaskUploader # Tells rails to use this uploader for this model.
   validates_presence_of :title, :description, :user_id, :assignee_id, :task_list_id
 end
