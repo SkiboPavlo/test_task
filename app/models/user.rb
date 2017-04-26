03e9f92cda3ca843539ac18fe2109ce34dc456b2
SkiboPavlo <skibo@i.ua>
2017-04-26 11:44:55 +0300

    add login with twitter and google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter]
   has_many :projects, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :assigned_to_tasks, class_name: 'Task', foreign_key: :assignee_id, source: :assignee
